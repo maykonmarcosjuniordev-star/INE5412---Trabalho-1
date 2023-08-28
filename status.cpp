8d09329790957a30a60f594bf39a430993c53d9e
clara2023 <clara2023@gmail.com>
2023-08-27 22:17:31 -0300

    Add files via upload

---
+++ status.cpp
@@ -0,0 +1,4 @@
+#include "iostream";
+void print_status (int execution_time, int ID, int current_state) {
+    std::cout << "t = " << execution_time << " ID = " << ID << " Estado = " << current_state << std::endl;
+}

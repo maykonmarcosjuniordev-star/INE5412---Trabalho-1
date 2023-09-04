b10bcab27c3bf68befe76da9072128f273d51187
clara2023 <clara2023@gmail.com>
2023-09-03 22:29:59 -0300

    funcao de execução CPU.hpp

--- CPU.hpp
+++ CPU.hpp
@@ -4,6 +4,10 @@ class CPU {
     public:
         CPU();
 
+        void exec(Process p) {
+            printf("Executando...\nProcesso %d executou por %ds",p.get_id(), (p.get_end_time()-p.get_start_time()));
+        }
+
     private:
         void preempt(Context new_context) {
 

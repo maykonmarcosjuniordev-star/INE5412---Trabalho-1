5dbdcbcc2f732ae4949a4bc09d3fad0dd7ea0563
clara2023 <clara2023@gmail.com>
2023-09-03 22:32:03 -0300

    Update ProcessQueue.hpp

--- ProcessQueue.hpp
+++ ProcessQueue.hpp
@@ -4,7 +4,17 @@
 class ProcessQueue {
     public:
         Process pop() {
-            
+            vector<Process *>::iterator it = queue.begin();
+            queue.erase(queue.begin());           
+        }
+        Process push(Process *p) {
+            queue.push_back(p);
+        }
+        int size() {
+            return queue.size();
+        }
+        Process* begin () {
+            return queue[0];
         }
     private:
         vector<Process *> queue;
